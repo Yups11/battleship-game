d56f114f461d313c078578b25c034e4b2fa59897
Elnur Nacu <yupsdigital@yahoo.com>
2024-07-17 10:01:59 +0000

    Create and setting the board

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+""" Board size 5 """
+board = []
+rc_number = 5
+
+for i in range(0, rc_number):
+    board.append(["0"] * rc_number)
+
+""" Print board """
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+print_board(board)
