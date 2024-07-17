2f19e04c51094b543910ad818b0adf03e43e7024
Elnur Nacu <yupsdigital@yahoo.com>
2024-07-17 18:01:24 +0000

    Set hide the batleships and battleship is ready

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 
 """ Board size 5 """
+
 board = []
 rc_number = 5
 
@@ -8,8 +9,23 @@ for i in range(0, rc_number):
     board.append(["0"] * rc_number)
 
 """ Print board """
+
 def print_board(board):
     for row in board:
         print(" ".join(row))
 
 print_board(board)
+
+""" Set hide the batleships and battleship is ready """
+
+def random_row(board):
+    return random.randrange(0, len(board))
+
+def random_col(board):
+    return random.randrange(0, len(board))
+
+ship_row_1 = random_row(board)
+ship_row_1 = random_col(board)
+
+ship_row_2 = random_row(board)
+ship_row_2 = random_col(board)
