5543ba4b8828819b1c623add717bc3fc2533e5e7
Elnur Nacu <yupsdigital@yahoo.com>
2024-07-17 18:55:13 +0000

    Write a while loop in order to avoid two the same points chosen in game area. Also write question to check name for player and print message to let now that game is starting

--- run.py
+++ run.py
@@ -24,8 +24,25 @@ def random_row(board):
 def random_col(board):
     return random.randrange(0, len(board))
 
-ship_row_1 = random_row(board)
-ship_row_1 = random_col(board)
 
+ship_row_1 = random_row(board)
+ship_col_1 = random_col(board)
+
+"""
+I comment this code because random point on game area can be the same for both players because is random for computer
 ship_row_2 = random_row(board)
-ship_row_2 = random_col(board)
+ship_col_2 = random_col(board)
+"""
+
+""" While loop in order to avoid two the same points chosen in game area"""
+ship_row_2, ship_col_2 = ship_row_1, ship_col_1
+
+while ship_row_2==ship_row_1:
+    ship_row_2 = random_row(board)
+
+while ship_col_2==ship_col_1:
+    ship_col_2 = random_col(board)
+
+print("Here we go!")
+player_1 = input("What is your name warrior ?: ")
+print(player_1, "Get ready, starting!!!\n")
