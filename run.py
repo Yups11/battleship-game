6c5141130adff3da6b70d2693b2ba9dcfb0f1b6a
Elnur Nacu <yupsdigital@yahoo.com>
2024-07-18 10:22:09 +0000

    Reformation of code due to small mistakes shown when check on PEP8 Python Validato

--- run.py
+++ run.py
@@ -29,7 +29,9 @@ ship_row_1 = random_row(board)
 ship_col_1 = random_col(board)
 
 """
-I comment this code because random point on game area can be the same for both players because is random for computer
+I comment this code because random point on game area can be the same for both
+players because is random for computer
+
 ship_row_2 = random_row(board)
 ship_col_2 = random_col(board)
 """
@@ -37,10 +39,10 @@ ship_col_2 = random_col(board)
 """ While loop in order to avoid two the same points chosen in game area"""
 ship_row_2, ship_col_2 = ship_row_1, ship_col_1
 
-while ship_row_2==ship_row_1:
+while ship_row_2 == ship_row_1:
     ship_row_2 = random_row(board)
 
-while ship_col_2==ship_col_1:
+while ship_col_2 == ship_col_1:
     ship_col_2 = random_col(board)
 
 print("Here we go!")
@@ -58,35 +60,34 @@ for turn in range(5):
     if (guess_row < 0 or guess_row > (rc_number-1)) or (guess_col < 0 or guess_col > (rc_number-1)):
         print("Values must be between 0 and 4!\n")
 
-    elif board[guess_row] [guess_col] == "*":
+    elif board[guess_row][guess_col] == "*":
         print("This point is already taken.\n")
 
     elif (guess_row == ship_row_1 and guess_col == ship_col_1) or (guess_row == ship_row_2 and guess_col == ship_col_2):
             hit_count = hit_count + 1
-            board[guess_row] [guess_col] = "*"
+            board[guess_row][guess_col] = "*"
             print("\nCongratulation!")
             if hit_count == 1:
-                   print("You hit first battleship!\n")
+                print("You hit first battleship!\n")
             elif hit_count == 2:
                 print("You hit second battleship!\n")
-                print_board(board)
-                break
+                print_board(board)        
     else:
-        if (board[guess_row] [guess_col]) == "X":
+        if (board[guess_row][guess_col]) == "X":
             print("You guessed that one already.\n")
         else:
             print("You missed my battleship!\n")
-            board[guess_row] [guess_col] = "X"
-        print (turn + 1, "turn")
+            board[guess_row][guess_col] = "X"
+        print(turn + 1, "turn")
     print_board(board)
 if hit_count == 2:
     print("Congratulations", player_1, "You win!!!")
 else:
     print("Sorry", player_1, "You Lose.")
-print ("Ship 1 is hidden:")
-print (ship_row_1)
-print (ship_col_1)
+print("Ship 1 is hidden:")
+print(ship_row_1)
+print(ship_col_1)
 
-print ("Ship 2 is hidden:")
-print (ship_row_2)
-print (ship_col_2)
+print("Ship 2 is hidden:")
+print(ship_row_2)
+print(ship_col_2)
